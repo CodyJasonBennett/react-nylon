593790b3955d82f357f3acd46d5fdc65ee46b1d1
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-06-24 04:15:43 -0500

    chore: microbundle => Vite

---
+++ vite.config.ts
@@ -0,0 +1,56 @@
+import * as path from 'node:path'
+import * as vite from 'vite'
+
+const mangleMap: Record<string, string> = {}
+
+export default vite.defineConfig({
+  root: process.argv[2] ? undefined : 'demo',
+  resolve: {
+    alias: {
+      'react-nylon': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    sourcemap: true,
+    target: 'es2020',
+    lib: {
+      formats: ['es', 'cjs'],
+      entry: 'src/index.ts',
+      fileName: '[name]',
+    },
+    rollupOptions: {
+      external: (id: string) => !id.startsWith('.') && !path.isAbsolute(id),
+      output: {
+        sourcemapExcludeSources: true,
+      },
+    },
+  },
+  plugins: [
+    {
+      name: 'vite-tsc',
+      generateBundle(_, bundle) {
+        if (bundle['index.mjs']) {
+          this.emitFile({ type: 'asset', fileName: 'index.d.ts', source: `export * from '../src'` })
+        }
+      },
+    },
+    {
+      name: 'vite-minify',
+      transform(code, url) {
+        if (!url.includes('node_modules')) {
+          for (const key in mangleMap) code = code.replaceAll(key, mangleMap[key])
+          return vite.transformWithEsbuild(code, url, {
+            mangleProps: /^_[A-Za-z]\w+/,
+            mangleQuoted: true,
+          })
+        }
+      },
+      renderChunk: {
+        order: 'post',
+        handler(code, { fileName }) {
+          return vite.transformWithEsbuild(code, fileName, { minify: true })
+        },
+      },
+    },
+  ],
+})
